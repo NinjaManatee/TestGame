89e6808c423b83f264177e7fa4d2749d05f86e04
NinjaManatee <andy@chartiq.com>
2017-02-24 16:34:46 -0500

    Updated documentaiton. Added moveBy().

--- src/js/Drawable.ts
+++ src/js/Drawable.ts
@@ -1,43 +1,89 @@
 /**
  * Creates the Drawable interface which will be the base class for all drawable objects in the game. Sets up default 
  * variables that all child objects will inherit, as well as the default functions.
+ * 
+ * @abstract
+ * @class Drawable
  */
 abstract class Drawable {
     /**
-     * The spead of the drawable object.
+     * Creates an instance of Drawable.
+     * 
+     * @param {number} x The x-position of the Drawable.
+     * @param {number} y The y-position of the Drawable.
+     * @param {number} height The height of the Drawable.
+     * @param {number} width The width of the Drawable.
+     * @param {number} canvasHeight The height of the canvas the Drawable is drawn on.
+     * @param {number} canvasWidth The width of the canvas the Drawable is drawn on.
+     * @param {number} speed The speed of th Drawable.
+      * 
+     * @memberOf Drawable
      */
-    speed: number;
-
-    /**
-     * The x-position of the canvas.
-     */
-    x: number;
-
-    /**
-     * The y-postion of the canvas.
-     */
-    y: number;
-
-    /**
-     * The width of the drawable object.
-     */
-    width: number;
-
-    /**
-     * The height of the drawable object.
-     */
-	height: number;
-
-    /**
-     * Initializes a new instance of the Drawable class.
-     */
-    public constructor(x: number, y: number) {
-        this.x = x;
-        this.y = y;
+    public constructor(
+        public readonly context: CanvasRenderingContext2D, 
+        public x: number, 
+        public y: number, 
+        public readonly height: number, 
+        public readonly width: number,
+        protected readonly canvasHeight: number,
+        protected readonly canvasWidth: number,
+        public speed: number) {
     }
 
     /**
      * Define abstract function to be implemented in child objects
+     * 
+     * @abstract
+     * 
+     * @memberOf Drawable
+     */
+    public abstract draw(): void;
+
+    /**
+	 * Moves the Drawable.
+	 * 
+	 * @private
+	 * @param {number} deltaX The amount to move in the x-direction.
+	 * @param {number} deltaY The amount to move in the y-direction.
+	 * 
+	 * @memberOf Drawable
 	 */
-    abstract draw(): void;
+	protected moveBy(deltaX: number, deltaY: number): void {
+		// The ship moved, so erase it's current image so it can
+		// be redrawn in it's new location
+		this.context.clearRect(this.x, this.y, this.width, this.height);
+			
+		let newX: number = this.x + deltaX;
+		let newY: number = this.y + deltaY;
+
+		this.draw();
+	}
+
+    public moveLeft() {
+// Update x and y according to the direction to move and
+			// redraw the ship. Change the else if's to if statements
+			// to have diagonal movement.
+			if (KEY_STATUS[KEY_CODE.Left]) {
+			this.x -= this.speed
+				if (this.x <= 0) {
+					// Keep player within the screen
+					this.x = 0;
+				}
+				} else if (KEY_STATUS[KEY_CODE.Right]) {
+				this.x += this.speed
+				if (this.x >= this.canvasWidth - this.width) {
+					this.x = this.canvasWidth - this.width;
+                }
+			} else if (KEY_STATUS[KEY_CODE.Up]) {
+				this.y -= this.speed
+				if (this.y <= this.canvasHeight/4*3) {
+					this.y = this.canvasHeight/4*3;
+                }
+			} else if (KEY_STATUS[KEY_CODE.Down]) {
+				this.y += this.speed
+				if (this.y >= this.canvasHeight - this.height) {
+					this.y = this.canvasHeight - this.height;
+                }
+			}
+	}
 }
